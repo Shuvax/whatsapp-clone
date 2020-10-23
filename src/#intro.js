50f32dc2ed9b4a734537e07ecfe55ed75b9201ca
Shuva <34592080+Shuvax@users.noreply.github.com>
2020-10-22 22:38:15 -0400

    First commit

---
+++ src/#intro.js
@@ -0,0 +1,10 @@
+// Tech in the build
+
+// React
+// Firebase Firestore Realtime db
+// Material UI
+// React Router
+// React Context API
+// ^^^ REDUX
+// Google Authentication
+// Deploy using Firebase
---
+++ src/firebase.js
@@ -0,0 +1,20 @@
+import firebase from "firebase";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyD_1fNVnB_jtaJWdB3-LhTypaJg09YJyQQ",
+    authDomain: "whatsapp-clone-16657.firebaseapp.com",
+    databaseURL: "https://whatsapp-clone-16657.firebaseio.com",
+    projectId: "whatsapp-clone-16657",
+    storageBucket: "whatsapp-clone-16657.appspot.com",
+    messagingSenderId: "509134281889",
+    appId: "1:509134281889:web:75149c6abd26716be8e4e0"
+  };
+
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+
+const db = firebaseApp.firestore();
+const auth = firebase.auth();
+const provider = new firebase.auth.GoogleAuthProvider();
+
+export { auth, provider };
+export default db;
